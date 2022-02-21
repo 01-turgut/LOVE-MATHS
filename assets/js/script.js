ab312451576e140566d82e5fbbe91b929646940e
01-turgut <gilgilturgut@gmail.com>
2022-02-21 13:40:39 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                
                 checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                
                 runGame(gameType)
             }
         })
@@ -35,6 +35,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2)
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -76,6 +78,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"]
+    }  else if (operator === "-") {
+        return [operand1 - operand2, "subtract"]
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`
@@ -107,8 +111,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
 
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operand1").textContent = operand1  > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand2 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 }
 
